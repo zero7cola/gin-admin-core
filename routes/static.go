f301397b34797c0339536e5a70ef7c0e40b5dd87
zero <zhangzhuang@dongyou.com>
2026-04-20 14:16:10 +0800

    4

--- routes/static.go
+++ routes/static.go
@@ -10,7 +10,13 @@ import (
 // RegisterStaticRoutes 注册 static 相关路由
 func RegisterStaticRoutes(r *gin.Engine) {
 
+	staticPath := "static"
+
+	if config.GetString("storage.local.static") != "" {
+		staticPath = config.GetString("storage.local.static")
+	}
+
 	// 本地文件
-	r.Static("/"+config.GetString("storage.local.static"), "storage/files")
+	r.Static("/"+staticPath, "storage/files")
 
 }
